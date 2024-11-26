16dea4dffb6aab3b5317884472138d87c9caa478
REALSTEVEIG <stephenignatiusbiz@gmail.com>
2024-11-26 20:51:24 +0100

    updates

--- build/app.js
+++ build/app.js
@@ -6,12 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const library_1 = __importDefault(require("./routes/library"));
-const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const swaggerConfig_1 = __importDefault(require("./config/swaggerConfig"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerConfig_1.default));
 app.use("/", library_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
